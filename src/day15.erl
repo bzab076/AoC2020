22bd0716f2b2432fef4ed110828eb70449f65168
bzab076 <bzab076@gmail.com>
2020-12-15 18:56:38 +0100

    day 15 added more comments

--- src/day15.erl
+++ src/day15.erl
@@ -24,19 +24,19 @@ getInput() -> [16,1,0,18,12,14,19].
 
 %% play memory game for turn N
 %% previously called numbers are stored in Dict, which is ets implementation of Set 
-play(N, LastElem, _, _, _, Iter) when N > Iter -> LastElem;
-play(N, _, List, ListSize, Dict, Iter) when N == 1 -> play(N+1, lists:nth(N, List), List, ListSize,  Dict, Iter);
-play(N, LastElem, List, ListSize, Dict, Iter) when N =<ListSize -> 
-	ets:insert(Dict, {LastElem, N-1}),
-	play(N+1, lists:nth(N, List), List, ListSize,  Dict, Iter );
+play(N, LastElem, _, _, _, Iter) when N > Iter -> LastElem; %% return result in final turn
+play(N, _, List, ListSize, Dict, Iter) when N == 1 -> play(N+1, lists:nth(N, List), List, ListSize,  Dict, Iter); %% call first number
+play(N, LastElem, List, ListSize, Dict, Iter) when N =<ListSize -> %% call numbers from the original list
+	ets:insert(Dict, {LastElem, N-1}), %% store previous number in set
+	play(N+1, lists:nth(N, List), List, ListSize,  Dict, Iter ); %% play next turn
 play(N, LastElem, List, ListSize, Dict, Iter) ->
-	    Val = ets:lookup(Dict, LastElem),
+	    Val = ets:lookup(Dict, LastElem), %% try to get last occurrence of previously spoken number
 		case length(Val) > 0 of
 			true -> {_, L} = hd(Val), NewVal = N - 1 - L;
 		    false -> NewVal = 0
 		end,
-        ets:insert(Dict, {LastElem, N-1}),
-		play(N+1, NewVal, List, ListSize, Dict, Iter).
+        ets:insert(Dict, {LastElem, N-1}), %% store previous number in set
+		play(N+1, NewVal, List, ListSize, Dict, Iter).  %% play next turn
 
 
 
